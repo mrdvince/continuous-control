1fdc0d868c9c57c2fc975aaadaa69e06459c63c9
Droid021 <vincivenv@gmail.com>
2020-05-23 13:46:27 +0000

    Trained agent

--- agent.py
+++ agent.py
@@ -10,17 +10,28 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 1024        # minibatch size
+BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 0.001         # learning rate of the actor
-LR_CRITIC = 0.001        # learning rate of the critic
-WEIGHT_DECAY = 0       # L2 weight decay
+LR_ACTOR = 1e-4         # learning rate of the actor
+LR_CRITIC = 1e-4        # learning rate of the critic
+WEIGHT_DECAY = 0.0      # L2 weight decay
+
+N_LEARN_UPDATES = 10    # number of learning updates
+N_TIME_STEPS = 20       # every n time step do update
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
+    memory = None
+    actor_local = None
+    actor_target = None
+    actor_optimizer = None
+
+    critic_local = None
+    critic_target = None
+    critic_optimizer = None
 
     def __init__(self, state_size, action_size, random_seed):
         """Initialize an Agent object.
@@ -34,45 +45,59 @@ class Agent():
         self.action_size = action_size
         self.seed = random.seed(random_seed)
 
-        # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
+        # initialize Class level Actor Network
+        if Agent.actor_local is None:
+            Agent.actor_local = Actor(state_size, action_size, random_seed).to(device)
+        if Agent.actor_target is None:
+            Agent.actor_target = Actor(state_size, action_size, random_seed).to(device)
+        if Agent.actor_optimizer is None:
+            Agent.actor_optimizer = optim.Adam(Agent.actor_local.parameters(), lr=LR_ACTOR)
+        self.actor_local = Agent.actor_local
+        self.actor_target = Agent.actor_target
+        self.actor_optimizer = Agent.actor_optimizer
 
-        # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        # Initilise Class levell Critic Network
+        if Agent.critic_local is None:
+            Agent.critic_local = Critic(state_size, action_size, random_seed).to(device)
+        if Agent.critic_target is None:
+            Agent.critic_target = Critic(state_size, action_size, random_seed).to(device)
+        if Agent.critic_optimizer is None:
+            Agent.critic_optimizer = optim.Adam(Agent.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        self.critic_local = Agent.critic_local
+        self.critic_target = Agent.critic_target
+        self.critic_optimizer = Agent.critic_optimizer
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
-        # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
-
-    def step(self, state, action, reward, next_state, done, t):
+        # Replay memory - only intitialise once per class
+        if Agent.memory is None:
+            Agent.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+            
+    def step(self, time_step, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
-        self.memory.add(state, action, reward, next_state, done)
+        Agent.memory.add(state, action, reward, next_state, done)
+
+        # only learn every n_time_steps
+        if time_step % N_TIME_STEPS != 0:
+            return
 
         # Learn, if enough samples are available in memory
-        if len(self.memory) > BATCH_SIZE and t % 20 == 0:
-            for i in range(10):
-                experiences = self.memory.sample()
+        if len(Agent.memory) > BATCH_SIZE:
+            for i in range(N_LEARN_UPDATES):
+                experiences = Agent.memory.sample()
                 self.learn(experiences, GAMMA)
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
         state = torch.from_numpy(state).float().to(device)
-
         self.actor_local.eval()
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
         self.actor_local.train()
-
         if add_noise:
-            for i in range(action.shape[0]):
-                action[i] += self.noise.sample()
+            action += self.noise.sample()
         return np.clip(action, -1, 1)
 
     def reset(self):
